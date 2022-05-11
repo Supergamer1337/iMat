7610feaa9658931b99d44dae3de4f7e7b5452ffd
Supergamer1337 <felix.b.aronsson@gmail.com>
2022-05-11 14:48:53 +0200

    Fixed hover on icon buttons

--- src/main/java/com/example/imat/IMatController.java
+++ src/main/java/com/example/imat/IMatController.java
@@ -1,8 +1,11 @@
 package com.example.imat;
 
+import com.example.imat.utils.StyleUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Product;
 
@@ -14,8 +17,24 @@ public class IMatController {
 
     IMatDataHandler dataHandler = IMatDataHandler.getInstance();
 
+    @FXML private ImageView shoppingCartImage;
+    private boolean shoppingCartIsHovered = false;
+
+    @FXML private ImageView profileImage;
+    private boolean profileIsHovered = false;
+
     @FXML
     public void initialize() {
         System.out.println("Current home path: " + System.getProperty("user.home"));
+    }
+
+    public void toggleShoppingCartHover() {
+        shoppingCartIsHovered = !shoppingCartIsHovered;
+        StyleUtils.toggleHoverImage(shoppingCartIsHovered, "icons/shopping-cart-hover.png", "icons/shopping-cart.png", shoppingCartImage);
+    }
+
+    public void toggleProfileHover() {
+        profileIsHovered = !profileIsHovered;
+        StyleUtils.toggleHoverImage(profileIsHovered, "icons/profile-hover.png", "icons/profile.png", profileImage);
     }
 }
---
+++ src/main/java/com/example/imat/utils/StyleUtils.java
@@ -0,0 +1,14 @@
+package com.example.imat.utils;
+
+import com.example.imat.IMat;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class StyleUtils {
+
+    public static void toggleHoverImage(boolean isHovered, String hoverPath, String normalPath, ImageView imageView) {
+        Image image = new Image(isHovered ? IMat.class.getResourceAsStream(hoverPath) : IMat.class.getResourceAsStream(normalPath));
+        imageView.setImage(image);
+    }
+
+}
