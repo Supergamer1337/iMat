ffb9d4c16e4fbdb616d384065f3479ae4c43775c
Supergamer1337 <felix.b.aronsson@gmail.com>
2022-05-14 10:12:28 +0200

    Better category images.

--- src/main/java/com/example/imat/utils/StyleUtils.java
+++ src/main/java/com/example/imat/utils/StyleUtils.java
@@ -3,6 +3,8 @@ package com.example.imat.utils;
 import com.example.imat.IMat;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
 
 public class StyleUtils {
     public static void toggleHoverImage(boolean isHovered, String hoverPath, String normalPath, ImageView imageView) {
@@ -10,4 +12,23 @@ public class StyleUtils {
         imageView.setImage(image);
     }
 
+    public static void roundBackgroundImage(AnchorPane imagePane, double amount ) {
+        Rectangle rect = new Rectangle(180,185);
+        rect.setArcHeight(amount);
+        rect.setArcWidth(amount);
+        imagePane.setClip(rect);
+    }
+
+    public static void roundBackgroundImage(AnchorPane imagePane) {
+        roundBackgroundImage(imagePane, 30.0);
+    }
+
+    public static void coverBackgroundImage(AnchorPane imagePane, String imagePath) {
+        imagePane.setStyle(
+                "-fx-background-image: url('" + imagePath + "');"
+                        + "-fx-background-size: auto 100%;"
+                        + "-fx-background-position: center center;"
+                        + "-fx-background-repeat: no-repeat;"
+        );
+    }
 }
