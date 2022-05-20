60f4541d6516fec49de2fe7003bd3ee9b511d22f
Supergamer1337 <felix.b.aronsson@gmail.com>
2022-05-20 09:26:01 +0200

    Can now edit faktureringsadress from profile

--- src/main/java/com/example/imat/IMatController.java
+++ src/main/java/com/example/imat/IMatController.java
@@ -112,6 +112,20 @@ public class IMatController implements ShoppingCartListener {
     @FXML private FlowPane searchPane;
     @FXML private ScrollPane searchBase;
 
+    @FXML private TextField profileDeliveryFirstname;
+    @FXML private TextField profileDeliveryLastname;
+    @FXML private TextField profileDeliveryAddress;
+    @FXML private TextField profileDeliveryPostalcode;
+    @FXML private TextField profileDeliveryCity;
+    @FXML private Button profileDeliverySaveButton;
+
+    @FXML private TextField profilePaymentName;
+    @FXML private TextField profilePaymentBank;
+    @FXML private TextField profilePaymentCardNumber;
+    @FXML private TextField profilePaymentDate;
+    @FXML private TextField profilePaymentCVC;
+    @FXML private Button profilePaymentSaveButton;
+
     @FXML
     public void initialize() {
         System.out.println("Current home path: " + System.getProperty("user.home"));
@@ -503,7 +517,29 @@ public class IMatController implements ShoppingCartListener {
     }
 
     @FXML public void showProfileInformation(){
+        fillProfileInformation();
+
         profileInformationPane.toFront();
+    }
+
+    private void fillProfileInformation() {
+        Customer savedCustomerData = dataHandler.getCustomer();
+
+        profileDeliveryFirstname.setText(savedCustomerData.getFirstName());
+        profileDeliveryLastname.setText(savedCustomerData.getLastName());
+        profileDeliveryAddress.setText(savedCustomerData.getAddress());
+        profileDeliveryPostalcode.setText(savedCustomerData.getPostCode());
+        profileDeliveryCity.setText(savedCustomerData.getPostAddress());
+    }
+
+    @FXML public void saveProfileInformation() {
+        Customer savedCustomerData = dataHandler.getCustomer();
+
+        savedCustomerData.setFirstName(profileDeliveryFirstname.getText());
+        savedCustomerData.setLastName(profileDeliveryLastname.getText());
+        savedCustomerData.setAddress(profileDeliveryAddress.getText());
+        savedCustomerData.setPostCode(profileDeliveryPostalcode.getText());
+        savedCustomerData.setPostAddress(profileDeliveryCity.getText());
     }
 
     @FXML
@@ -587,11 +623,6 @@ public class IMatController implements ShoppingCartListener {
         int total = (int)dataHandler.getShoppingCart().getTotal();
         shoppingCartCounterLabel.setText(String.valueOf(total));
     }
-
-
-
-
-
 
     private void addToLocationHistory(LocationInfo currentLocation, boolean addToHistory) {
         removeLocationHighlight();
